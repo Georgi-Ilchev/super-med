db8a5d1cfda721ac8e5ca62e2be9f69c00e85d7e
NikolaiStoilkov <37143199+NikolaiStoilkov@users.noreply.github.com>
2022-02-12 18:52:11 +0200

    fixed email exist

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -9,155 +9,169 @@ import './Register.css';
 import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
 
 const Register = () => {
-    const navigate = useNavigate();
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [passwordError, setpasswordError] = useState("");
-    const [emailError, setemailError] = useState("");
-    const [allUsers, setAllUser] = useState(null);
+  const navigate = useNavigate();
 
-    const [rePass, setRePass] = useState("");
-    const [rePassError, setRePassError] = useState("");
+  const [email, setEmail] = useState('');
 
-    const handleValidation = (event) => {
-        let formIsValid = true;
+  const [rePass, setRePass] = useState('');
 
-        if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
-            formIsValid = false;
-            setemailError("Email Not Valid");
-            return false;
-        } else {
-            setemailError("");
-            formIsValid = true;
-        }
+  const [password, setPassword] = useState('');
 
-        if (!password.match(/^[a-zA-Z]{8,22}$/)) {
-            formIsValid = false;
-            setpasswordError(
-                "Only Letters and length must best min 8 Chracters and Max 22 Chracters"
-            );
-            return false;
-        } else {
-            setpasswordError("");
-            formIsValid = true;
-        }
+  const [emailError, setEmailError] = useState('');
 
-        if (password != rePass) {
-            setRePassError(
-                "Password dont match!"
-            );
-            return false;
-        } else {
-            setRePassError("");
-            formIsValid = true;
-        }
+  const [rePassError, setRePassError] = useState('');
 
-        return formIsValid;
-    };
+  const [formIsValid, setFormIsValid] = useState(true);
 
-    const onRegisterSubmit = async (e) => {
-        e.preventDefault();
-        handleValidation(e);
+  const [existingUser, setExistingUser] = useState(null);
 
-        const email = e.target.email.value;
-        const password = e.target.password.value;
+  const [passwordError, setPasswordError] = useState('');
 
-        const users = collection(db, 'users');
-        const q = query(users, where("email", "==", email));
-        const dataUsers = await getDocs(q);
+  const handleValidation = () => {
 
-        // console.log(dataUsers);
+    if (!email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      setFormIsValid(false);
+      setEmailError('Email Not Valid');
+    } else {
+      setEmailError('');
+      setFormIsValid(false);
+    }
 
-        if (!dataUsers.empty) {
-            setemailError("Email already exist!");
-            return [];
-        }
+    if (!password.match(/^[a-zA-Z]{8,22}$/)) {
+      setFormIsValid(false);
 
-        setAllUser(dataUsers.docs.map(u => console.log(u)));
+      setPasswordError(
+        'Only Letters and length must best min 8 Chracters and Max 22 Chracters'
+      );
+      return false;
+    } else {
+      setPasswordError('');
+      setFormIsValid(false);
+    }
 
-        try {
-            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+    if (password !== rePass) {
+      setRePassError(
+        'Password dont match!'
+      );
+      setFormIsValid(false);
 
-            await setDoc(doc(db, "users", userCredentials.user.uid), {
-                email,
-            });
+    } else {
+      setRePassError('');
+      setFormIsValid(false);
 
-            navigate("/");
+    }
+  };
 
-        } catch (error) {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        }
-    };
+  const onRegisterSubmit = async (e) => {
+    e.preventDefault();
+    handleValidation();
 
-    return (
-        <section className="register-section">
-            <div className="App">
-                <div className="container">
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-md-4">
-                            <form id="loginform" onSubmit={onRegisterSubmit}>
-                                <div className="form-group">
-                                    <label>Email address</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="EmailInput"
-                                        name="email"
-                                        aria-describedby="emailHelp"
-                                        placeholder="Enter email"
-                                        onChange={(event) => setEmail(event.target.value)}
-                                    />
-                                    <small id="emailHelp" className="text-danger form-text">
-                                        {emailError}
-                                    </small>
-                                </div>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="exampleInputPassword1"
-                                        name="password"
-                                        placeholder="Password"
-                                        onChange={(event) => setPassword(event.target.value)}
-                                    />
-                                    <small id="passworderror" className="text-danger form-text">
-                                        {passwordError}
-                                    </small>
-                                </div>
-                                <div className="form-group">
-                                    <label>Repeat Password</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="exampleInputPassword1"
-                                        name="re-password"
-                                        placeholder="Password"
-                                        onChange={(event) => setRePass(event.target.value)}
-                                    />
-                                    <small id="passworderror" className="text-danger form-text">
-                                        {rePassError}
-                                    </small>
-                                </div>
-                                <div className="form-group form-check">
-                                    <input
-                                        type="checkbox"
-                                        className="form-check-input"
-                                        id="exampleCheck1"
-                                    />
-                                    <label className="form-check-label">Check me out</label>
-                                </div>
-                                <button type="submit" className="btn btn-primary">
-                                    Register
-                                </button>
-                            </form>
-                        </div>
-                    </div>
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const users = collection(db, 'users');
+    const q = query(users, where('email', '==', email));
+    const dataUsers = await getDocs(q);
+
+    if (!dataUsers.empty) {
+      setEmailError('Email already exist!');
+      return [];
+    }
+
+    if (!formIsValid) {
+      return;
+    }
+
+    dataUsers.docs.map(u => setExistingUser(u));
+
+    if (existingUser) {
+      setEmailError('This user already exist!');
+      return;
+    }
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+      await setDoc(doc(db, 'users', userCredentials.user.uid), {
+        email
+      });
+
+      navigate('/');
+
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    }
+  };
+
+  return (
+    <section className="register-section">
+      <div className="App">
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-4">
+              <form id="loginform" onSubmit={onRegisterSubmit}>
+                <div className="form-group">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="EmailInput"
+                    name="email"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                  <small id="emailHelp" className="text-danger form-text">
+                    {emailError}
+                  </small>
                 </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    name="password"
+                    placeholder="Password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                  <small id="passworderror" className="text-danger form-text">
+                    {passwordError}
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label>Repeat Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    name="re-password"
+                    placeholder="Password"
+                    onChange={(event) => setRePass(event.target.value)}
+                  />
+                  <small id="passworderror" className="text-danger form-text">
+                    {rePassError}
+                  </small>
+                </div>
+                <div className="form-group form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              </form>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
